7a319e2dde448cfdbdd0dfa3aa469593ad34e6cd
gba <blacko952@gmail.com>
2014-04-21 18:47:41 +0200

    Apprentissage Entity System

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+    class MaList : List<List<Object>>
+    {
+
+        public void Add(List<Object> l)
+        {
+            base.Add(l);
+        }
+
+        public bool Remove(List<Object> l)
+        {
+            return base.Remove(l);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            MaList list = new MaList();
+            list.Add(new List<Object>());
+            list.Add(new List<Object>());
+            list.Remove(new List<Object>());
+            foreach (List<Object> o in list)
+            {
+                Console.WriteLine(o);
+            }
         }
     }
 }
---
+++ WindowsGame1/WindowsGame1/WindowsGame1/Wall.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace WindowsGame1
+{
+    class Wall
+    {
+        public static Texture2D texture;
+        public Rectangle hitbox;
+        Color color;
+
+        //public Wall(int x, int y, Texture2D texture)
+        public Wall(int x, int y, int size, Color color)
+        {
+            //Wall.texture = texture;
+            this.color = color;
+            hitbox = new Rectangle(x, y, size, size);
+        }
+
+        // CONSTRUCTOR
+
+        // METHODS
+        public static void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("pixel");
+        }
+
+        // UPDATE AND DRAW
+        public void Update(MouseState mouse, KeyboardState keyboard)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(texture, this.hitbox, color);
+        }
+    }
+}
